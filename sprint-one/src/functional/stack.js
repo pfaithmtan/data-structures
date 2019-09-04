a11281758cc242bda4b4ca766384321995fb2bef
Faith <pfaithmtan@gmail.com>
2019-09-04 12:04:14 -0700

    Implement size()
    - Declare variable to store default stack size 0
    - Return variable
    - Add to stack size within push()
    - Subtract from stack size within pop()
    - Keep time complexity constant

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,22 @@
 var Stack = function() {
   var someInstance = {};
 
+  someInstance.stackSize = 0;
+
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.stackSize += 1;
   };
 
   someInstance.pop = function() {
+    someInstance.stackSize -= 1;
   };
 
   someInstance.size = function() {
+    return someInstance.stackSize;
   };
 
   return someInstance;
