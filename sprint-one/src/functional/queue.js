59b0dd21a8f13f1d6a07b772c7eaf82164d07c89
Faith <pfaithmtan@gmail.com>
2019-09-04 14:58:41 -0700

    Implement size()
    - Declare variable that has default size 0
    - Return variable
    - Keep time and complexity constant

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,6 +1,8 @@
 var Queue = function() {
   var someInstance = {};
 
+  someInstance.queueSize = 0;
+
   // Use an object with numeric keys to store values
   var storage = {};
 
@@ -13,6 +15,7 @@ var Queue = function() {
   };
 
   someInstance.size = function() {
+    return someInstance.queueSize;
   };
 
   return someInstance;
