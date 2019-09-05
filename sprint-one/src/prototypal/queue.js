57c274a0cf30f59c18582ba6f506f578f2c56259
Faith <pfaithmtan@gmail.com>
2019-09-04 21:25:51 -0700

    Change variable declaration from const to var
    - Change on line 5
    - Pass declare variable spec

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -2,7 +2,7 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 
-  const newQueue = Object.create(queueMethods);
+  var newQueue = Object.create(queueMethods);
 
   newQueue.queueSize = 0;
   newQueue.firstIdx = 0;
