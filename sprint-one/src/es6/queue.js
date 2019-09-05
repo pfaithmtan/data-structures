d3ea9197ae0c667cff8f916213c6879209df744e
Faith <pfaithmtan@gmail.com>
2019-09-04 20:44:45 -0700

    Complete constructor
    - Include variables for size and first and last indices

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,20 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.queueSize = 0;
+    this.firstIdx = 0;
+    this.lastIdx = 0;
   }
 
+  enqueue() {
+
+  }
+
+  dequeue() {
+
+  }
+
+  size() {
+
+  }
 }
