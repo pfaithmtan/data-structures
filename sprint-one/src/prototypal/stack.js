e6cea9def528ffa97318aa06243f10594ad932e7
Faith <pfaithmtan@gmail.com>
2019-09-04 21:24:59 -0700

    Change variable declaration from const to var
    - Change on line 5
    - Pass declare variable spec

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -2,7 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 
-  const newStack = Object.create(stackMethods);
+  var newStack = Object.create(stackMethods);
 
   newStack.stackSize = 0;
 
