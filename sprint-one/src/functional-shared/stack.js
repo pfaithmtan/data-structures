bb6b08ca3d0a39b47032a31e354ff3495244d722
Faith <pfaithmtan@gmail.com>
2019-09-04 17:17:09 -0700

    Implement size and push method
    - Declare new stack object in Stack function
    - Declare variableto keep track of stack size
    - Write extend method
    - Initialise stack methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  const newStack = {};
+  newStack.stackSize = 0;
+  extend(newStack, stackMethods);
+
+  return newStack;
 };
 
-var stackMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+var stackMethods = {
+  push: function(value) {
+    this[this.stackSize] = value;
+
+    this.stackSize += 1;
+  },
+
+  pop: function() {
+
+  },
+
+  size: function() {
+    return this.stackSize;
+  }
+};
 
 
