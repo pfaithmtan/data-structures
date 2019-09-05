174bdcf7a0747e881417edeaeab61229e0736317
pfaithmtan <pfaithmtan@gmail.com>
2019-09-05 16:15:55 -0700

    Complete LinkedList
    - Implement .addToTail()
    - Implement .removeHead()
    - Implement .contains() via recursive methodology

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,45 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let newNode = Node(value);
+
+    // Make sure we already have a head, tail
+    if (!list.head) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      if (list.head === list.tail) {
+        list.tail = null;
+      }
+      // Get former head, set head to next node or null
+      const oldHead = list.head;
+      list.head = list.head.next;
+      return oldHead.value;
+    }
   };
 
   list.contains = function(target) {
+    recursive = function(currNode, target) {
+      if (currNode.value === target) {
+        return true;
+      }
+      if (currNode.next === null) {
+        return false;
+      }
+      return recursive(currNode.next, target);
+    };
+    if (list.head) {
+      return recursive(list.head, target);
+    }
+    return false;
   };
-
   return list;
 };
 
