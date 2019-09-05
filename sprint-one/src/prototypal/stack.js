a1f91c4dae77f5f197b559c07881c47a23a5ce9e
Faith <pfaithmtan@gmail.com>
2019-09-04 20:21:24 -0700

    Implement Stack using prototypal instantiation

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  const newStack = Object.create(stackMethods);
+
+  newStack.stackSize = 0;
+
+  return newStack;
 };
 
-var stackMethods = {};
+const stackMethods = {
+  push: function(value) {
+    this[this.stackSize] = value;
+
+    this.stackSize += 1;
+  },
+
+  pop: function() {
+    const lastEl = this[this.stackSize - 1];
+
+    delete this[this.stackSize - 1];
+
+    if (this.stackSize > 0) {
+      this.stackSize -= 1;
+    }
+
+    return lastEl;
+  },
+
+  size: function() {
+    return this.stackSize;
+  }
+};
 
 
