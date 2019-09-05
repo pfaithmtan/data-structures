7c4714a9ede8430eaecb1ad22ad57de2e6297455
Faith <pfaithmtan@gmail.com>
2019-09-04 19:42:28 -0700

    Initialise Queue function and methods
    - Include extend function
    - Declare variable for indices and size

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  const newQueue = {};
+  newQueue.queueSize = 0;
+  newQueue.firstIdx = 0;
+  newQueue.lastIdx = 0;
+  extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
+var queueMethods = {
+  enqueue: function(value) {
+
+  },
+
+  dequeue: function() {
+
+  },
+
+  size: function() {
+
+  }
+};
 
