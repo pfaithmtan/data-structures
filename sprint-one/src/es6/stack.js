0d274b0dfcb883799478d6974dc92cdb2867247a
Faith <pfaithmtan@gmail.com>
2019-09-04 20:40:12 -0700

    Implement constructor
    - Declare variable to keep track of size where default is 0
    - Complete size method

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,18 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.stackSize = 0;
   }
 
+  push(value) {
+
+  }
+
+  pop() {
+
+  }
+
+  size() {
+    return this.stackSize;
+  }
 }
