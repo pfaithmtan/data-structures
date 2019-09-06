7ac0552be4fec1a109177e57659b66b486d54001
pfaithmtan <pfaithmtan@gmail.com>
2019-09-05 22:03:22 -0700

    Complete Tree
    - Implement .addChild()
    - Implement .contains() recursively
    - Employ variable to ensure traversal of all nodes

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -13,9 +13,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  const child = Tree(value);
+
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  if (this.children.length) {
+    for (let i = 0; i < this.children.length; i++) {
+      let doesContain = this.children[i].contains(target);
+
+      if (doesContain) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
 
 
