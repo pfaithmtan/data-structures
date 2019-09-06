5ff17f4abe823fccfe438fef4a02acf9022d3eb7
pfaithmtan <pfaithmtan@gmail.com>
2019-09-06 11:21:56 -0700

    Complete Set Implementation
    - Implement Set.add()
    - Implement Set.contains()
    - Implement Set.remove()

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
+
+  // Add methods from setPrototype to set
+  set.add = setPrototype.add;
+  set.contains = setPrototype.contains;
+  set.remove = setPrototype.remove;
+
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // add item to object
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  // use hasOwnProperty to see if item is in object
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  // delete item from object
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Set.add(): O(1)
+ Set.contains(): O(1)
+ Set.remove(): O(1)
  */
