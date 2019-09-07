f8a0c4f0e76a04864b1b4f776d3bc0f3b4b24a71
pfaithmtan <pfaithmtan@gmail.com>
2019-09-07 16:51:47 -0700

    Complete Advanced Content
    - Implement HashTable.increaseSize() using fill-ratio of 0.625
    - Implement HashTable.reduceSize()

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,6 +5,90 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+HashTable.prototype.recreateStorage = function() {
+  // Loop over storage and return new array that mimics storage
+  const newStorage = [];
+  this._storage.each((element) => {
+    newStorage.push(element);
+  });
+  return newStorage;
+};
+
+HashTable.prototype.getPercentFull = function() {
+  // Record space taken
+    // Loop through storage
+      // if !undefined and obj.keys.length
+        // increment space taken
+  // return space taken / this._limit
+
+  let spaceTaken = 0;
+  const currStorage = this.recreateStorage();
+
+  for (let i = 0; i < currStorage.length; i++) {
+    if (currStorage[i] && Object.keys(currStorage[i]).length) {
+      spaceTaken++;
+    }
+  }
+
+  return spaceTaken / this._limit;
+};
+
+HashTable.prototype.reduceSize = function() {
+  // set limit = floor(limit / 2)
+  // store old storage as variable
+  // create, set new storage
+  // loop through old storage
+    //  if !undefined and obj.keys.length
+      // insert into storage
+
+  const oldStorage = this.recreateStorage();
+  this._limit = Math.floor(this._limit / 2);
+  this._storage = LimitedArray(this._limit);
+
+  for (let i = 0; i < oldStorage.length; i++) {
+    if (oldStorage[i] && Object.keys(oldStorage[i]).length) {
+      const obj = oldStorage[i];
+      for (let j = 0; j < Object.keys(obj).length; j++) {
+        const k = Object.keys(obj)[j];
+        const v = obj[k];
+        this.insert(k, v);
+      }
+    }
+  }
+};
+
+
+HashTable.prototype.increaseSize = function() {
+  // Check if space taken is greater than 0.75 * limit
+  // set limit = floor(limit * 2)
+  // store old storage as variable
+  // create, set new storage
+  // loop through old storage
+    //  if !undefined and obj.keys.length
+      // insert into storage
+
+  const oldStorage = this.recreateStorage();
+  this._limit = Math.floor(this._limit * 2);
+  this._storage = LimitedArray(this._limit);
+
+  for (let i = 0; i < oldStorage.length; i++) {
+    if (oldStorage[i] && Object.keys(oldStorage[i]).length) {
+      const obj = oldStorage[i];
+      for (let j = 0; j < Object.keys(obj).length; j++) {
+        const k = Object.keys(obj)[j];
+        const v = obj[k];
+        this.insert(k, v);
+      }
+    }
+  }
+};
+
+HashTable.prototype.logStorage = function() {
+  this._storage.each((element, index, storage) => {
+    console.log(storage);
+  });
+};
+
 HashTable.prototype.insert = function(k, v) {
   // Get index for insertion
   var index = getIndexBelowMaxForKey(k, this._limit);
@@ -17,6 +101,11 @@ HashTable.prototype.insert = function(k, v) {
   obj[k] = v;
   // Set obj into storage
   this._storage.set(index, obj);
+
+  // if necessary, resize storage
+  if (this.getPercentFull() >= 0.625) {
+    this.increaseSize();
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -44,6 +133,11 @@ HashTable.prototype.remove = function(k) {
   }
   // Set obj into storage
   this._storage.set(index, obj);
+
+  // if necessary, resize storage
+  if (this.getPercentFull() < 0.25) {
+    this.reduceSize();
+  }
 };
 
 
